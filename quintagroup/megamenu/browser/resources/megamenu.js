33f80ac76110672140702983bb4078c32b8d2c2b
Yuriy Hvozdovych <gvizdyk@quintagroup.com>
2014-08-12 16:38:18 +0300

    Fixed style

---
+++ quintagroup/megamenu/browser/resources/megamenu.js
@@ -0,0 +1,18 @@
+jQuery(window).load(function() {
+
+    $( "[id$='globalnav']" ).addClass("megamenu");
+	$( ".navbar-toggle" ).click(function() {
+			$( "#nav" ).slideToggle(500)
+            $( "[id$='globalnav']" ).toggleClass( "open" );
+		});
+
+    $( "#nav li .subs").has( "ol" ).parent("#nav li").addClass("plus");
+
+    $("#nav > li.plus > a").click(function (event) {
+        if ($(window).width() <= 768){
+            event.preventDefault();
+            $("#nav > li").not($(this).parent()).removeClass("open").find(".subs").slideUp(350);
+            $(this).parent().toggleClass("open").find(".subs").slideToggle(500);
+            }
+        });
+    });
