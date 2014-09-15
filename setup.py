1c6a399fc069e8dc05d391ce2152b17a1aee6e18
Roman Kozlovskyi <kroman0@users.noreply.github.com>
2014-09-15 12:23:52 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,36 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.2'
+version = '1.3'
 
 setup(name='quintagroup.megamenu',
       version=version,
       description="Mega menu for Plone",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.rst")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
+          "Framework :: Plone",
+          "Framework :: Plone :: 3.2",
+          "Framework :: Plone :: 3.3",
+          "Framework :: Plone :: 4.0",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Plone :: 4.3",
+          "Framework :: Zope2",
+          "Framework :: Zope3",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+          "Operating System :: OS Independent",
+          "Development Status :: 5 - Production/Stable",
+      ],
+      keywords='menu mega panels',
+      author='Quintagroup',
+      author_email='support@quintagroup.com',
+      url='https://github.com/quintagroup/quintagroup.megamenu/',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['quintagroup'],
