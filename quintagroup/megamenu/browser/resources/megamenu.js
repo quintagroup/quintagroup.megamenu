119b9b304ff1add1bbf49cb273ff9a8179bd0e32
Yuriy Hvozdovych <gvizdyk@quintagroup.com>
2015-03-30 17:53:10 +0000

    formated code in 'megamenu.js'

--- quintagroup/megamenu/browser/resources/megamenu.js
+++ quintagroup/megamenu/browser/resources/megamenu.js
@@ -1,17 +1,14 @@
 jQuery(window).load(function() {
-
-	$( ".navbar-toggle" ).click(function() {
-			$( "#portal-globalnav" ).slideToggle(500)
-            $( "#nav" ).toggleClass( "open" );
-		});
-
-    $( "#portal-globalnav li .subs").parent("#portal-globalnav li").addClass("plus");
-
-    $("#portal-globalnav > li.plus > a").click(function (event) {
-        if ($(window).width() <= 768){
+    $(".navbar-toggle").click(function() {
+        $("#portal-globalnav").slideToggle(500)
+        $("#nav").toggleClass("open");
+    });
+    $("#portal-globalnav li .subs").parent("#portal-globalnav li").addClass("plus");
+    $("#portal-globalnav > li.plus > a").click(function(event) {
+        if ($(window).width() <= 768) {
             event.preventDefault();
             $("#portal-globalnav > li").not($(this).parent()).removeClass("open").find(".subs").slideUp(350);
             $(this).parent().toggleClass("open").find(".subs").slideToggle(500);
-            }
-        });
+        }
     });
+});
