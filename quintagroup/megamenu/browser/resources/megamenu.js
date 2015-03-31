aebfb84a31b792c7742f10e4b5b35a6a170bb73c
roman.ischiv <roman.ischiv@quintagroup.org>
2015-03-31 16:09:51 +0300

    Fixed js

--- quintagroup/megamenu/browser/resources/megamenu.js
+++ quintagroup/megamenu/browser/resources/megamenu.js
@@ -1,14 +1,21 @@
 jQuery(window).load(function() {
     $(".navbar-toggle").click(function() {
-        $("#portal-globalnav").slideToggle(500);
+        $(".megamenu-nav").slideToggle(500);
         $("#portal-megamenu").toggleClass("open");
     });
-    $("#portal-globalnav li .subs").parent("#portal-globalnav li").addClass("plus");
-    $("#portal-globalnav > li.plus > a").click(function(event) {
+    $(".megamenu-nav li .subs").parent(".megamenu-nav li").addClass("plus");
+    $(".megamenu-nav > li.plus > a").click(function(event) {
         if ($(window).width() <= 768) {
             event.preventDefault();
-            $("#portal-globalnav > li").not($(this).parent()).removeClass("open").find(".subs").slideUp(350);
+            $(".megamenu-nav > li").not($(this).parent()).removeClass("open").find(".subs").slideUp(350);
             $(this).parent().toggleClass("open").find(".subs").slideToggle(500);
+        }
+    });
+    $(window).resize(function() {
+        if ($(window).width() >= 768) {
+            $(".megamenu-nav").css({'display': 'block'})
+        } else {
+            $("#portal-megamenu:not(.open) .megamenu-nav").css({'display': 'none'})
         }
     });
 });
